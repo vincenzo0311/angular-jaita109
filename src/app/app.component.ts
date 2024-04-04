fe59477019db8bc3bb9adbfcc421923e546e0497
esteban.cpp <stefano.iannone9603@gmail.com>
2024-04-04 11:24:55 +0200

    Implementato checkLogin su areadirigenti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'scuola-angular';
+  isMainPage = false;
+
+  constructor(private router : Router){
+    //Con questo codice controlliamo se siamo 'atterrati' sulla pagina principale (cioe': "/")
+    this.router.events.subscribe(evento =>{
+      if(evento instanceof NavigationEnd){
+        this.isMainPage = evento.urlAfterRedirects === "/"
+      }
+    })
+  }
 }
--- src/app/areadirigenti/areadirigenti.component.ts
+++ src/app/areadirigenti/areadirigenti.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./areadirigenti.component.css']
 })
 export class AreadirigentiComponent {
+  constructor(private http : HttpClient){
+    this.http = http;
+    this.checkLogin();
+  }
 
+
+
+  checkLogin(){
+    var token = sessionStorage.getItem("token");
+    if(token == null){
+      alert("NON HAI EFFETTUATO UN LOGIN VALIDO")
+      sessionStorage.clear();
+      window.location.href="/";
+    }
+    else{
+      //Compongo gli headers della richiesta, inserendo il token e il ruolo che mi aspetto 
+      //(in questo caso DIRIGENTE perchè sto accedendo alla pagina dei dirigenti)
+      const headers = new HttpHeaders(
+        {
+          'Content-Type' : 'application/json',
+          'token' : token,
+          'role' : 'DIRIGENTE'
+        }
+      );
+
+      this.http.get("http://localhost:8080/api/login/checklogin", {headers}).subscribe(risposta =>{
+        let check = risposta as boolean;
+        if(!check){
+          alert("Non sei autorizzato ad accedere a questa pagina")
+          window.location.href="/";
+        }
+      })
+    }
+  }
 }
