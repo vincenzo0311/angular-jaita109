3c1f17a72314a0f90740a60f780866eceaaaac2e
esteban.cpp <stefano.iannone9603@gmail.com>
2024-04-04 17:56:41 +0200

    implementazione delete studente, fine giornata 04-04-24

--- src/app/areadirigenti/tabellastudenti/tabellastudenti.component.ts
+++ src/app/areadirigenti/tabellastudenti/tabellastudenti.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { Studente } from 'src/models/Studente';
 
@@ -9,4 +10,42 @@ import { Studente } from 'src/models/Studente';
 export class TabellastudentiComponent {
   //Ricevo il vettore di studenti dal componente areadirigenti grazie a @Input
   @Input() studenti? : Studente[];
+
+  constructor(private http : HttpClient){
+    this.http = http;
+  }
+
+
+  deleteStudente(id : number){
+    let token = sessionStorage.getItem("token");
+    if(token == null){
+      token = "";
+    }
+
+    const headers = new HttpHeaders(
+      {
+        'Content-Type' : 'application/json',
+        'token': token as string
+      }
+    );
+
+    const params = new HttpParams().set('idStudente', id);
+    
+    this.http.get<boolean>("http://localhost:8080/api/studente/delete", {headers, params}).subscribe(risposta =>{
+      if(risposta){
+        alert("eliminazione avvenuta con successo");
+        // window.location.href="/areadirigenti";
+
+        //Cerco la posizione dell'elemento con l'id cercato
+        //findIndex() eseguirà un'iterazione su tutti gli elementi del vettore applicando il codice che gli ho scritto
+        //dopo la freccetta (=>) se quel codice da come risultato 'true', findIndex() si interrempe 
+        //e ritorna la posizione nel vettore in cui si trovava
+        let pos = this.studenti?.findIndex(x => x.id === id)
+        if(pos! > -1){
+          this.studenti?.splice(pos!, 1);
+        }
+      }
+    })
+
+  }
 }
